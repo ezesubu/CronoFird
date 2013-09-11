79dd688bdb36c1305f54455d0922610ec9eb287f
Ezequiel Suárez <ezesubu@gmail.com>
2013-09-11 08:44:35 -0500

    upload xls to db

--- core/application/controllers/app.php
+++ core/application/controllers/app.php
@@ -8,8 +8,7 @@ class app extends CI_Controller {
       
     }
 
-    public function test(){
-        
+    public function test(){        
         $this->load->model("Test_mdl");
         $arrData['user_name'] = "Ezequiel Suarez Buitrago";
 
@@ -19,15 +18,61 @@ class app extends CI_Controller {
             ToJSONMsg("ERR", $e->getMessage());
             return;
         }
-
-
         $this->load->view('test/test'); 
     }
+
     public function ingresar(){
-        echo "funciono";
+        echo $_POST;
+    }
+
+    //Render view to upload file
+    public function xls_to_db(){
+        $this->load->view('app/xls_to_db');
+    }
+    //method to upload file
+    public function upload_file(){
+        #$this->load->library('excel_reader2.php');    
+        $strFilePath = NULL;
+        var_dump($_FILES);
+        if ( !empty($_FILES['userfile']['tmp_name']) ){
+            $strFilePath = $_FILES['userfile']['tmp_name'];
+        }    
+        
+        $test = $this->_readFile($strFilePath);
+        var_dump($test);
+
+    }
+
+
+    private function _readFile( $strFilePath )
+    {
+      require_once(APPPATH . '/libraries/ExcelReader.php');
+      try {
+       
+       $objReader = new allusExcelReader();
+      
+       $arrConfig['strFilePath'] = $strFilePath;
+       
+       $arrConfig['arrFieldNames'] = array(
+        'posicion',
+        'nombre',
+        'No',
+        'Edad',
+        'tiempo_oficial',
+        'paso'
+       );
+      
+       $objReader->setConfig($arrConfig);
+      
+       $arrData = $objReader->actionReadFile();
+       
+      } catch ( Exception $e ) {
+       throw $e;
+      }
+      
+      
+      return $arrData;
+      
     }
 
 }
-
-/* End of file welcome.php */
-/* Location: ./application/controllers/welcome.php */
